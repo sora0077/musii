1ebcabdba999db460b509832d64a43eccdfb8009
sora0077 <t.hayashi0077+github@gmail.com>
2018-11-22 20:39:07 +0900

    Add ApplicationService.framework

---
+++ ApplicationService/ApplicationService.h
@@ -0,0 +1,19 @@
+//
+//  ApplicationService.h
+//  ApplicationService
+//
+//  Created by 林達也 on 2018/11/22.
+//  Copyright © 2018 林達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApplicationService.
+FOUNDATION_EXPORT double ApplicationServiceVersionNumber;
+
+//! Project version string for ApplicationService.
+FOUNDATION_EXPORT const unsigned char ApplicationServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApplicationService/PublicHeader.h>
+
+
